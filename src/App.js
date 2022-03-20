6603277eccc7a57a9c4deeb1fa66811e9336b7c8
iRekila <ilkka.rekila@gmail.com>
2022-03-20 22:45:49 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,63 @@
-import logo from './finnishbearbeer_logo.svg';
+import logo from './fg.png';
+import wheat from './wheat.jpg'
 import './App.css';
 
 function App() {
   return (
     <html>
       <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-<link href="https://fonts.googleapis.com/css2?family=Concert+One" rel="stylesheet"></link>
-</head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link href="https://fonts.googleapis.com/css2?family=Concert+One" rel="stylesheet"></link>
 
-<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        </header>
-    </div>
-<header id="header"class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      <ul id="header_text" class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" class="nav-link px-2 link-secondary">HOME</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">MARKET</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">DRINKS</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">CONTACT</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">ABOUT BEER</a></li>
-        
-      </ul>
-      <div id="header_text"class="col-md-3 text-end">
-        <button type="button" class="btn btn-warning" id="cart_button">CART</button>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link href="https://fonts.googleapis.com/css2?family=Concert+One" rel="stylesheet"></link>
+      </head>
+
+
+      <div class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <img src={logo} class="App-logo" alt="logo" />
       </div>
-    </header>
+      <div class="container-fluid">
+        <header id="header" class="">
+          <ul id="header_text" class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="#" class="px-2">HOME</a></li>
+            <li><a href="#" class="px-2">MARKET</a></li>
+            <li><a href="#" class="px-2">DRINKS</a></li>
+            <li><a href="#" class="px-2">CONTACT</a></li>
+            <li><a href="#" class="px-2">ABOUT BEER</a></li>
+          </ul>
+        </header>
+      </div>
 
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <img src={wheat} id="wheat"></img>
+          </div>
+          <div id="div_text" class="col-sm">
+            <h1>WHO WE ARE</h1>
+            <a> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+              nonummy nibh euismod tincidunt
+              ut laoreet dolore magna aliquam
+              erat volutpat. Ut wisi enim ad
+              minim veniam, quis nostrud exerci
+              tation ullamcorp
+            </a>
+            <div id="readmore_div"><button type="button" id="readmore">READ MORE</button></div>
+          </div>
+        </div>
+      </div>
+      <footer class="bg-dark text-white text-center text-lg-start fixed-bottom">
 
+        <div id="footer" class="text-center p-3" >
+          © 2022 Copyright: sg3
+          <a class="text-dark"></a>
+        </div>
 
-
-</html>
+      </footer>
+    </html>
   );
 }
 
