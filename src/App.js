b58871dda9f3fadf5c4d290182f3660158efc611
iRekila <ilkka.rekila@gmail.com>
2022-03-23 14:24:50 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -7,22 +7,23 @@ import Readmore from './pages/readmore';
 import Nav from './components/nav';
 import Footer from './components/footer';
 import Headers from './components/headers';
+import NotFound from './pages/notfound';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
-return (
-  
-      <div className='container'>
-        <Headers />
-        <Nav />
-        <Footer />
-        <Routes>
-          <Route path="/" element={<Front />} />
-          <Route path="/market" element={<Market />} />
-          <Route path="/drinks" element={<Drinks />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<Readmore />} />
+  return (
+    <div className='container'>
+      <Headers/>
+      <Nav/>
+      <Footer />
+      <Routes>
+        <Route path="/" element={<Front />} />
+        <Route path="/market" element={<Market />} />
+        <Route path="/drinks" element={<Drinks />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<Readmore />} />
+        <Route path="/*" element={<NotFound />} />
         </Routes>
       </div>
   );
---
+++ src/pages/notfound.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <html>
+        
+        <h1 id="otsikot">PAGE NOT FOUND</h1>
+
+    </html>
+  )
+}
