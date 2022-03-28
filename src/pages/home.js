e8fe7d9407469dc439654893c259a3a7f14bc7c7
iRekila <ilkka.rekila@gmail.com>
2022-03-28 14:06:58 +0300

    jeee

--- src/pages/home.js
+++ src/pages/home.js
@@ -23,11 +23,6 @@ export default function Front() {
             </a>
             <div class="container">
               <Link to="/readmore"><button type="button" id="readmore" data-toggle="collapse" data-target="#textbox">READ MORE</button></Link>
-              <div id="textbox" class="collapse">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              </div>
             </div>
           </div>
         </div>
