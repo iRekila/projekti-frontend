42276b6318e48768755a1a5316c700d28a94a1f9
kristaiida <92725476+kristaiida@users.noreply.github.com>
2022-03-29 19:55:47 +0300

    Added viewing of all products. Theres a bug about access control

--- src/components/products.js
+++ src/components/products.js
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function Products({ url, addToCart }) {
-    const [categoryName, seCategoryName] = useState('');
+    const [categoryName, setCategoryName] = useState('');
     const [products, setProducts] = useState([]);
+
+    let params = useParams();
+
+    useEffect(() => {
+        axios.get(url + 'products/getproducts.php' + params.categoryId)
+            .then((response) => {
+                const json = response.data;
+                setCategoryName(json.category);
+                setProducts(json.products);
+            }).catch(error => {
+                alert(error.response === undefined ? error : error.response.data.error);
+            })
+    }, [params])
+
 
     return (
         <div>
             <h3>Products for {categoryName}</h3>
             {products.map(product => (
-                <div key={product.id}>
-                    {product.name}
+                <div key={product.tuotenro}>
+                    {product.tuotenimi}
                     <button className="btn btn-primary" type="button" onClick={e => addToCart(product)}>Add</button>
                 </div>
             ))}
