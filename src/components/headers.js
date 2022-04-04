5d326588b35985df652e60937a7a36d444887d37
Noora Turpeinen <turpeinen.n@hotmail.com>
2022-04-04 15:23:43 +0300

    edited footer

--- src/components/headers.js
+++ src/components/headers.js
@@ -1,6 +1,6 @@
 export default function Headers() {
     return (
-        <head>
+        <div>
             <link rel="preconnect" href="https://fonts.googleapis.com"></link>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"></link>
             <link href="https://fonts.googleapis.com/css2?family=Concert+One" rel="stylesheet"></link>
@@ -8,7 +8,6 @@ export default function Headers() {
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
                 integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
                 crossOrigin="anonymous"></script>
-            <script src="https://kit.fontawesome.com/dbbfcc22fb.js" crossOrigin="anonymous"></script>
-        </head>
+        </div>
     )
 }
