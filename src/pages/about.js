889719c311ba224b8b6fd06ebbff385d06b7cb97
kristaiida <92725476+kristaiida@users.noreply.github.com>
2022-04-04 15:54:42 +0300

    Little fixes

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,7 @@ export default function About() {
             <h1>ABOUT BEER</h1>
               <p id="text3">Beer, it's the best damn drink in the world. -Jack Nicholson</p>
               <br></br>
-              <p id="text4"> You know the best thing about craft beers? The variety of beer styles and flavours guarantees that everyone can find their favourite one. We chose the four different styles of craft beer because in our opinion they showcase different kinds of flavours really well. Lager is the most common around the world so it’s easy to start your tasting journey there and move on to different styles too. We are a group of six people who’s dream it was to bring domestic craft beers to everyone and since you have to chase your dreams, we just went ahead and did it. We hope you’ll enjoy them just as much as we do! Feedback gives us a chance to do better so we highly encourage you to do so. Just click ’CONTACT’ on the navigation bar.
+              <p id="text4"> You know the best thing about craft beers? The variety of beer styles and flavours guarantees that everyone can find their favourite one. We chose the four different styles of craft beer because in our opinion they showcase different kinds of flavours really well. Lager is the most common around the world so it’s easy to start your tasting journey there and move on to different styles too. We are a group of six people who’s dream it was to bring domestic craft beers to everyone and since you have to chase your dreams, we just went ahead and did it. We hope you’ll enjoy them just as much as we do! Feedback gives us a chance to do better so we highly encourage you to do so by just clicking ’CONTACT’ on the navigation bar.
               </p>
           </div>
         </div>
