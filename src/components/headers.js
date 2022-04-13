811b912e661f4502c3dbe121485b33c95a72e7b6
Noora Turpeinen <turpeinen.n@hotmail.com>
2022-04-13 12:56:31 +0300

    fixed crossorigin

--- src/components/headers.js
+++ src/components/headers.js
@@ -7,7 +7,7 @@ export default function Headers() {
             <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans" rel="stylesheet"></link>
             
             <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anymous"></link>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anymous"></link>
             <link href="https://fonts.googleapis.com/css2?family=Concert+One&family=Josefin+Sans:ital,wght@0,300;0,400;1,300&display=swap" rel="stylesheet"></link>
 
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
