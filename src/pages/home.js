6bd61ec5414d838cfbc0d791ab21ba57acb95364
Noora Turpeinen <turpeinen.n@hotmail.com>
2022-04-13 14:06:57 +0300

    fixed layout

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,11 +12,11 @@ export default function Front() {
               <img src={wheat} id="wheat"></img>
             </div>
             <div className="col-sm" id="div_text" style={{ paddingLeft: "0", paddingRight: "0" }}>
-              <h1 style={{ marginBottom: "0.5em" }}>WHO WE ARE</h1>
+              <h1 style={{ marginBottom: "1em" }}>WHO WE ARE</h1>
               <a id="text1">
                 Finnish Bear Beer is a company providing domestic craft brewery products. We offer constantly changing products for people who are just as passionate about supporting domestic companies as we are. 
               </a>
-              <div className="container" id="rd">
+              <div className="container" id="rd" style={{ marginTop: "0.5em" }}>
                 <Link to="/readmore"><button type="button" id="readmore" data-toggle="collapse" data-target="#textbox">READ MORE</button></Link>
               </div>
             </div>
--- src/pages/readmore.js
+++ src/pages/readmore.js
@@ -6,12 +6,12 @@ export default function Readmore() {
   return (
     <div className="container">
         <div className="row">
-          <div id="div_text" className="col-sm" style={{ paddingLeft: "0", paddingRight: "0" }}>
-            <h1>WHO WE ARE</h1>
+          <div id="div_text" className="col-sm" style={{ marginTop: "1.5em", marginLeft: "4em", marginRight: "4em", paddingLeft: "0", paddingRight: "0" }}>
+            <h1 style={{ marginBottom: "1.5em" }}>WHO WE ARE</h1>
             <a id="text2"> We were just a group of people who enjoyed craft beers and started to wonder if there would be a market to bring domestic craft beers to every beer lovers doorstep and there begun the idea for Finnish Bear Beer. <br></br><br></br>
             Our main goal with Finnish Bear Beer is to bring a large variety of different domestic craft brewery products for everyone to enjoy. You can choose craft beers from four different categories which are Pils, Pale Ale, Wheat beer and Lager. Every single one of the beers in our constantly changing selection is extraordinary in their own way and you can choose the ones for you to try. Join the community and start tasting exceptional craft beers!
             </a>
-            <div className="container" id="rd">
+            <div className="container" id="rd" style={{ marginTop: "1em" }}>
               <Link to="/"><button type="button" id="readmore" data-toggle="collapse" data-target="#textbox">READ LESS</button></Link>
             </div>
           </div>
